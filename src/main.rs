e0bad831e1e33414e4ab78b67e801cb978d55718
Leo Ferreira <leonardoferreira@protonmail.com>
2023-03-02 22:27:21 -0300

    print resquests

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use server::Server;
 
-mod server;
 mod http;
+mod server;
 
 fn main() {
     let string = String::from("127.0.0.1:8080");
--- src/server.rs
+++ src/server.rs
@@ -1,19 +1,39 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
-  address: String,
+    address: String,
 }
 
 impl Server {
-  pub fn new(address: String) -> Self {
-      Server {
-          address
-      }
-  }
+    pub fn new(address: String) -> Self {
+        Server { address }
+    }
 
-  pub fn run(self) {
-      let listener = TcpListener::bind(&self.address).unwrap();
+    pub fn run(self) {
+        let listener = TcpListener::bind(&self.address).unwrap();
 
-      println!("running!!! on {}", self.address);
-  }
+        println!("running!!! on {}", self.address);
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(error) => {
+                            println!("some error on read stream {}", error);
+                        }
+                    }
+                }
+                Err(_) => {
+                    println!("error")
+                }
+            }
+
+        }
+    }
 }
