c941df1d40f4b117ddc56915da24c3bde04629a1
Leo Ferreira <leonardoferreira@protonmail.com>
2023-03-05 22:58:49 -0300

    feat(request): create Error type

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,65 @@
 use super::method::Method;
+use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
 
 pub struct Request {
-  path: String,
-  query_string: Option<String>,
-  method: Method,
+    path: String,
+    query_string: Option<String>,
+    method: Method,
 }
+
+impl TryFrom<&[u8]> for Request {
+    type Error = ParseError;
+
+    // request: GET / HTTP/1.1
+    // Host: 127.0.0.1:8080
+    // User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0
+    // Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+    // Accept-Language: en-US,en;q=0.5
+    // Accept-Encoding: gzip, deflate, br
+    // DNT: 1
+    // Connection: keep-alive
+    // Upgrade-Insecure-Requests: 1
+    // Sec-Fetch-Dest: document
+    // Sec-Fetch-Mode: navigate
+    // Sec-Fetch-Site: none
+    // Sec-Fetch-User: ?1
+    // Sec-GPC: 1
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        unimplemented!()
+    }
+}
+
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(f, "{}", self.message())
+    }
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "invalid request",
+            Self::InvalidEncoding => "invalid encoding",
+            Self::InvalidProtocol => "invalid protocol",
+            Self::InvalidMethod => "invalid method",
+        }
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,7 @@
+use crate::http::Request;
 use std::io::Read;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -23,6 +26,13 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("request: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer as &[u8]) {
+                                Ok(req) =>{},
+                                Err(e) => println!("failed to parse a request: {}", e)
+                            }
+
+                            let res: &Result<Request, _> = &buffer[..].try_into();
                         }
                         Err(error) => {
                             println!("some error on read stream {}", error);
@@ -33,7 +43,6 @@ impl Server {
                     println!("error")
                 }
             }
-
         }
     }
 }
