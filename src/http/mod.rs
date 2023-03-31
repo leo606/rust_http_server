88b56f84d584bd9c06daa58652601b9c65325d30
Leo Ferreira <leonardoferreira@protonmail.com>
2023-03-31 07:53:06 -0300

    response struct

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -2,7 +2,9 @@ pub use method::Method;
 pub use request::Request;
 pub use request::ParseError;
 pub use query_string::{QueryString, QueryStringValue};
+pub use response::Response;
 
 pub mod method;
 pub mod request;
 pub mod query_string;
+pub mod response;
