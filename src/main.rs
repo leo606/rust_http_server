bbeb88bcec9d99d736f83a8b39706105f9de4e5b
Leo Ferreira <leonardoferreira@protonmail.com>
2023-04-26 08:06:07 -0300

    public path

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,17 @@
 
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::{env};
 
 mod http;
 mod server;
 pub mod website_handler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR")); // reads de variables at compiletime
     let string = String::from("127.0.0.1:8080");
-
     let server = Server::new(string);
-    server.run(WebsiteHandler)
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path); // reads the variables at runtime
+    println!("public_path => {}", public_path);
+    server.run(WebsiteHandler::new(public_path))
 }
