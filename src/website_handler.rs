d50a5ede6fb6d1dd4e2e9515d0615e903a83b681
Leo Ferreira <leonardoferreira@protonmail.com>
2023-04-27 14:00:56 -0300

    serve arbitrary files securely

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -13,8 +13,18 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        println!("{}", path);
-        fs::read_to_string(path).ok()
+
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory transversal attack attempted: '{}'", file_path);
+                    None
+                }
+            },
+            Err(_) => None,
+        }
     }
 }
 
